2867afc8374742eb20ede8a2b3d0c0d7dd6fdbf4
Ramez Gerges <es-Ramez.Adly2023@alexu.edu.eg>
2023-03-14 01:59:33 +0200

    Synthetic images: YOLO output

--- synthetic_ramez.py
+++ synthetic_ramez.py
@@ -42,10 +42,10 @@ arabic_diacritics = re.compile("""
                              """, re.VERBOSE)
 
 
-def cleanup():
-    if Path("./output").exists():
-        shutil.rmtree("./output")
-    Path("./output").mkdir(exist_ok=True)
+def cleanup(out_dir):
+    if out_dir.exists():
+        shutil.rmtree(out_dir)
+    out_dir.mkdir(exist_ok=True)
 
 
 def normalize_arabic(text):
@@ -85,11 +85,17 @@ def rotate(image, boxes, angle, resize=True):
 
 
 if __name__ == "__main__":
-    DEBUG = True
+    OUT_DIR = Path("./output")
+    DEBUG = False
+    REBUILD = True
     N = 5000
-    cleanup()
-
+    TRAIN = 0.7
+    VAL = 0.2
+    TEST = 0.1
     random.seed(0)
+
+    if REBUILD:
+        cleanup(OUT_DIR)
 
     sizes = list(range(40, 100))
     rotations = list(range(-10, 10))
@@ -108,74 +114,125 @@ if __name__ == "__main__":
     words = map(remove_diacritics, words)
     words = list(words)
 
-    images = []
-    tqd = tqdm.tqdm(total=N)
-    while len(images) < N:
-        tqd.update()
+    unique_letters = list(set("".join(words)))
+    letter_to_class = {letter: i for i, letter in enumerate(unique_letters)}
 
-        word_not_reshaped = random.choice(words)
-        size = random.choice(sizes)
-        rotation = random.choice(rotations)
-        font_filename = random.choice(fonts)
-        padding_left = random.randint(*padding_left_range)
-        padding_right = random.randint(*padding_right_range)
-        padding_top = random.randint(*padding_top_range)
-        padding_bot = random.randint(*padding_bot_range)
+    if REBUILD:
+        images = []
+        tqd = tqdm.tqdm(total=N)
+        while len(images) < N:
+            tqd.update()
 
-        word = arabic_reshaper.reshape(word_not_reshaped)
-        font = ImageFont.truetype(font_filename, size)
-        box = font.getbbox(word)
+            word_not_reshaped = random.choice(words)
+            size = random.choice(sizes)
+            rotation = random.choice(rotations)
+            font_filename = random.choice(fonts)
+            padding_left = random.randint(*padding_left_range)
+            padding_right = random.randint(*padding_right_range)
+            padding_top = random.randint(*padding_top_range)
+            padding_bot = random.randint(*padding_bot_range)
 
-        width = box[2] - box[0] + padding_left + padding_right
-        height = box[3] - box[1] + padding_top + padding_bot
-        x1, y1 = (-box[0] + padding_left, -box[1] + padding_top)
+            word = arabic_reshaper.reshape(word_not_reshaped)
+            font = ImageFont.truetype(font_filename, size)
+            box = font.getbbox(word)
 
-        with TTFont(font_filename, 0, ignoreDecompileErrors=True) as ttf:
-            if any([ord(c) not in ttf.getBestCmap() for c in word]):
-                continue
-            widths = [ttf.getGlyphSet().hMetrics[ttf.getBestCmap()[ord(c)]][0] for c in word]
-        sum_ = sum(widths)
-        widths = [w * (box[2] - box[0]) / sum(widths) for w in widths][::-1]
+            width = box[2] - box[0] + padding_left + padding_right
+            height = box[3] - box[1] + padding_top + padding_bot
+            x1, y1 = (-box[0] + padding_left, -box[1] + padding_top)
 
-        img = Image.new("L", (width, height), "white")
-        draw = ImageDraw.Draw(img)
-        draw.text((x1, y1), word, "black", font=font)
+            with TTFont(font_filename, 0, ignoreDecompileErrors=True) as ttf:
+                if any([ord(c) not in ttf.getBestCmap() for c in word]):
+                    continue
+                widths = [ttf.getGlyphSet().hMetrics[ttf.getBestCmap()[ord(c)]][0] for c in word]
+            sum_ = sum(widths)
+            widths = [w * (box[2] - box[0]) / sum(widths) for w in widths][::-1]
 
-        offset = 0
-        boxes = []
-        for w in widths:
-            box = (padding_left + offset, padding_top, padding_left + offset + w, padding_top + box[3] - box[1])
-            offset += w
-            boxes.append(box)
-
-        if DEBUG:
-            img.save(f"./output/{len(images)}.png")
-
-        np_img, boxes = rotate(np.array(img) ^ 255, boxes, rotation)
-        np_img ^= 255
-        np_bytes = BytesIO()
-        np.save(np_bytes, np_img)
-        images.append({
-            "image": np_bytes.getvalue(),
-            "boxes": boxes,
-            "ground_truth": word_not_reshaped
-        })
-
-        if DEBUG:
-            img = Image.fromarray(np_img)
+            img = Image.new("L", (width, height), "white")
             draw = ImageDraw.Draw(img)
-            for box in boxes:
-                draw.line((box[0], box[1], box[2], box[3]), fill="black")
-                draw.line((box[2], box[3], box[4], box[5]), fill="black")
-                draw.line((box[4], box[5], box[6], box[7]), fill="black")
-                draw.line((box[6], box[7], box[0], box[1]), fill="black")
-            img.save(f"./output/{len(images) - 1}_rotated.png")
+            draw.text((x1, y1), word, "black", font=font)
 
-    with open("./output/data.pickle", "wb") as f:
-        pickle.dump(images, f)
+            offset = 0
+            boxes = []
+            for w in widths:
+                box = (padding_left + offset, padding_top, padding_left + offset + w, padding_top + box[3] - box[1])
+                offset += w
+                boxes.append(box)
+
+            if DEBUG:
+                img.save(f"./output/{len(images)}.png")
+
+            np_img, boxes = rotate(np.array(img) ^ 255, boxes, rotation)
+            np_img ^= 255
+            np_bytes = BytesIO()
+            np.save(np_bytes, np_img)
+            images.append({
+                "image": np_bytes.getvalue(),
+                "boxes": boxes,
+                "ground_truth": word_not_reshaped
+            })
+
+            if DEBUG:
+                img = Image.fromarray(np_img)
+                draw = ImageDraw.Draw(img)
+                for box in boxes:
+                    draw.line((box[0], box[1], box[2], box[3]), fill="black")
+                    draw.line((box[2], box[3], box[4], box[5]), fill="black")
+                    draw.line((box[4], box[5], box[6], box[7]), fill="black")
+                    draw.line((box[6], box[7], box[0], box[1]), fill="black")
+                img.save(f"./output/{len(images) - 1}_rotated.png")
+
+        with open("./output/data.pickle", "wb") as f:
+            pickle.dump(images, f)
 
     with open("./output/data.pickle", "rb") as f:
         images = pickle.load(f)
-        for i in range(len(images)):
-            images[i]["image"] = np.load(BytesIO(images[i]["image"]), allow_pickle=True)
-        Image.fromarray(images[0]["image"]).save("./output/test_pickle.png")
+
+        train_dir = OUT_DIR.joinpath("train")
+        val_dir = OUT_DIR.joinpath("val")
+        test_dir = OUT_DIR.joinpath("test")
+        image_dir_name = "images"
+        label_dir_name = "labels"
+
+        for dir in [train_dir, val_dir, test_dir]:
+            dir.mkdir(exist_ok=False)
+            dir.joinpath(label_dir_name).mkdir(exist_ok=False)
+            dir.joinpath(image_dir_name).mkdir(exist_ok=False)
+
+        for i, img_dict in enumerate(images):
+            if i / N < TRAIN:
+                dir = train_dir
+            elif i / N < TRAIN + VAL:
+                dir = val_dir
+            else:
+                dir = test_dir
+
+            img = np.load(BytesIO(img_dict["image"]), allow_pickle=True)
+            boxes = img_dict["boxes"]
+            label = img_dict["ground_truth"]
+
+            # normalization
+            for box in boxes:
+                box[0] /= img.shape[1]
+                box[2] /= img.shape[1]
+                box[4] /= img.shape[1]
+                box[6] /= img.shape[1]
+
+                box[1] /= img.shape[0]
+                box[3] /= img.shape[0]
+                box[5] /= img.shape[0]
+                box[7] /= img.shape[0]
+
+            filename = f"{i}"
+            Image.fromarray(img).save(dir.joinpath(image_dir_name, filename + ".png"))
+            boxes_str = [f"{letter_to_class[c]} {box[0]} {box[1]} {box[2]} {box[3]} {box[4]} {box[5]} {box[6]} {box[7]}\n"
+                for box, c in zip(boxes, label[::-1])]
+            with open(dir.joinpath(label_dir_name, filename + ".txt"), "w") as f:
+                f.writelines(boxes_str)
+
+        output_str = "names:\n"
+        for l in unique_letters:
+            output_str += f"  - {ord(l)}\n"
+        output_str += f"nc: {len(unique_letters)}\n"
+
+        with open("./output/data.yaml", "w") as f:
+            f.write(output_str)

5b447f1a38a3fdbdcb8f21b31deda667d40fed71
astroC86 <66444189+astroC86@users.noreply.github.com>
2023-04-20 22:17:01 +0200

    Minor change

--- combined.py
+++ combined.py
@@ -13,20 +13,26 @@ def load_image(img_path: str):
 
 
 word_model = YOLO("./word_model.pt")
+
+
 def infer_words(img: np.ndarray):
     global word_model
-    # returns (word_image, bb, class)
+    # returns List[(word_image, bb, class)]
     results = word_model.predict(img)
     returnable = []
-    while len(results[0])!=0:
-      for r in results:
-          boxes = r.boxes
-          for box in boxes:
-              x1, y1, x2, y2 = box.xyxy[0].cpu().data.numpy().astype(int).tolist()
-              returnable.append((img[y1:y2, x1:x2].copy(), box,  word_model.names[int(box.cls)]))
-              img = cv2.rectangle(img,(x1,y1),(x2,y2),(255,255,255),-1)
-      results = word_model.predict(img)
+    while len(results[0]) != 0:
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0].cpu(
+                ).data.numpy().astype(int).tolist()
+                returnable.append(
+                    (img[y1:y2, x1:x2].copy(), box,  word_model.names[int(box.cls)]))
+                img = cv2.rectangle(img, (x1, y1), (x2, y2),
+                                    (255, 255, 255), -1)
+        results = word_model.predict(img)
     return returnable
+
 
 def infer_letters(img: np.ndarray):
     letter_model.predict(img)

b3d2c8fbf656a314834f9cce3ff5fa3c26cfb64e
Ramez Gerges <es-Ramez.Adly2023@alexu.edu.eg>
2023-05-10 23:19:28 +0300

    Combined models: cleanup and deskewing/otsu

--- combined.py
+++ combined.py
@@ -1,14 +1,18 @@
 import os
 import re
+import sys
 from pathlib import Path
 
 import cv2
 import numpy as np
-from PIL import Image
 from more_itertools import split_when
 from ultralytics import YOLO
 from ultralytics.yolo.utils.plotting import Annotator
 from datasets import load_metric
+from deskew import determine_skew
+from jdeskew.utility import rotate
+
+from synthetic_words_ramez import cleanup
 
 word_model = YOLO("./word_model.pt")
 letter_model = YOLO("./letter_model.pt")
@@ -41,6 +45,7 @@ name_to_unicode = {
     'waw-hamza': 1572,
     'tha': 1579,
     'taa-': 1591,
+    'thaa-': ord("ث"),
     'ha': 1726,
     '7a': 1581,
     'baa-3dot': 1662,
@@ -70,18 +75,17 @@ def infer_words(img: np.ndarray):
     # returns List[(word_image, bb, class)]
     results = word_model.predict(img, verbose=False)
     returnable = []
-    while len(results[0]) != 0:
-        for r in results:
-            boxes = r.boxes
-            for box in boxes:
-                x1, y1, x2, y2 = box.xyxy[0].cpu(
-                ).data.numpy().astype(int).tolist()
-                returnable.append(
-                    (img[y1:y2, x1:x2].copy(), box, word_model.names[int(box.cls)]))
-                img = cv2.rectangle(img, (x1, y1), (x2, y2),
-                                    (255, 255, 255), -1)
-        results = word_model.predict(img)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0].cpu(
+            ).data.numpy().astype(int).tolist()
+            returnable.append(
+                (img[y1:y2, x1:x2].copy(), box, word_model.names[int(box.cls)]))
+            img = cv2.rectangle(img, (x1, y1), (x2, y2),
+                                (255, 255, 255), -1)
     return returnable
+
 
 
 def preprocess_box(img):
@@ -92,19 +96,21 @@ def preprocess_box(img):
     return img
 
 
-def infer_letters(img: np.ndarray, conf=0.5):
-    results = letter_model.predict(img, conf=conf, verbose=False)[0]
+def infer_letters(img: np.ndarray, conf=0.5, model=letter_model, convert_names=False):
+    results = model.predict(img, conf=conf, verbose=False)[0]
     boxes = [box.xyxy[0].tolist() for box in results.boxes]  # x1, y1, x2, y2
     xs = [box[2] for box in boxes]
     sorted_indices = np.argsort(xs)[::-1]  # sort by max x2
 
     letters = [int(results.boxes.cls[i]) for i in sorted_indices]
-    letters = [letter_model.names[class_no] for class_no in letters]
-    letters = [name_to_unicode[name] for name in letters]
-    letters = [chr(unc) for unc in letters]
+    letters = [model.names[class_no] for class_no in letters]
+
+    if convert_names:
+        letters = [name_to_unicode[name] for name in letters]
+        letters = [chr(unc) for unc in letters]
+
     word = "".join(letters)
     return word
-
 
 def save_to_file():
     pass
@@ -146,36 +152,41 @@ def evaluate_yolo_model(test_dir, model):
     cer_metric = load_metric("cer")
     return cer_metric.compute(predictions=predicted_texts, references=ground_truths)
 
-if __name__ == '__main__':
-    print(evaluate_yolo_model("/home/ramez/PycharmProjects/Datagenerator/output/train", word_model))
 
 # if __name__ == '__main__':
-#     img_path = "./cropped2.jpg"
-#     # img_path = "/home/ramez/IMG-20230321-WA0017.jpg"
-#
-#     img = load_image(img_path)
-#     word_boxes = infer_words(img)[3:4]
-#
-#
-#     def box_to_key(box, thresh):
-#         x1, y1, x2, y2 = box[1].xyxy[0].tolist()
-#         return y1, -x2
-#
-#
-#     word_boxes = groupbyrow(word_boxes)
-#     word_boxes = [i for sublist in word_boxes for i in
-#                   sorted(sublist, key=lambda b: b[1].xyxy[0].tolist()[2], reverse=True)]
-#
-#     word_imgs = [x[0] for x in word_boxes]
-#     word_imgs = [preprocess_box(img) for img in word_imgs]
-#     word_texts = [infer_letters(x) for x in word_imgs]
-#     print(" ".join(word_texts))
-#
-#     # image = cv2.cvtColor(cv2.imread("/home/ramez/PycharmProjects/Datagenerator/cropped2.jpg"), cv2.COLOR_BGR2RGB)
-#     # image = cv2.cvtColor(cv2.imread("/home/ramez/IMG-20230321-WA0017.jpg"), cv2.COLOR_BGR2RGB)
-#     # res = infer_words(image.copy())
-#     # annotator = Annotator(image, line_width=1)
-#     # for i, r in enumerate(res):
-#     #     annotator.box_label(r[1].xyxy[0].cpu(), str(i))
-#     # frame = annotator.result()
-#     # cv2.imwrite("lmao.png", frame)
+#     print(evaluate_yolo_model("/home/ramez/PycharmProjects/Datagenerator/output/train", word_model))
+
+if __name__ == '__main__':
+    cleanup(Path("./output"))
+
+    for iter, img_path in enumerate(sys.argv[1:]):
+        img = cv2.imread(img_path)
+        cv2.imwrite(f"./output/{iter}_orig_img.png", img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        img = rotate(img, determine_skew(img), border_mode=cv2.BORDER_CONSTANT, border_value=255)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        cv2.imwrite(f"./output/{iter}_filtered_img.png", img)
+
+        img_before_inference = np.copy(img)
+        word_boxes = infer_words(img)
+        annotator = Annotator(img_before_inference, line_width=1)
+        for i, r in enumerate(word_boxes):
+            annotator.box_label(r[1].xyxy[0].cpu(), str(i))
+        frame = annotator.result()
+        cv2.imwrite(f"./output/{iter}_labeled_img.png", img_before_inference)
+
+
+        def box_to_key(box, thresh):
+            x1, y1, x2, y2 = box[1].xyxy[0].tolist()
+            return y1, -x2
+
+
+        word_boxes = groupbyrow(word_boxes)
+        word_boxes = [i for sublist in word_boxes for i in
+                      sorted(sublist, key=lambda b: b[1].xyxy[0].tolist()[2], reverse=True)]
+
+        word_imgs = [x[0] for x in word_boxes]
+        word_imgs = [preprocess_box(img) for img in word_imgs]
+        word_texts = [infer_letters(x, convert_names=True) for x in word_imgs]
+        print(" ".join(word_texts))
